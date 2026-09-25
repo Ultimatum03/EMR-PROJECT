04f00d752f3c8e8730b60cf089983fff465011eb
praisenikeadedokun <praisenikeadedokun@gmail.com>
2026-09-25 14:13:35 +0100

    changes made

--- emr-data.sql
+++ emr-data.sql
@@ -16,9 +16,9 @@ CREATE TABLE patients (
     next_of_kin VARCHAR(100) NOT NULL,
     next_of_kin_phone VARCHAR(20) NOT NULL,
     next_of_kin_relationship VARCHAR(50) NOT NULL,
-    next_of_kin_address TEXT NOT NULL
+    next_of_kin_address TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at DATETIME2 NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 /* Create the users table */
